cfdcdc77dac753ee0d6346b79e8f02722c27d183
Dhruvil189 <67578611+Dhruvil189@users.noreply.github.com>
2022-06-15 15:23:52 +0530

    Update settings.py

--- jsn/jsn/settings.py
+++ jsn/jsn/settings.py
@@ -141,6 +141,6 @@ MEDIA_URL='/media/'
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
-EMAIL_HOST_USER = 'jantasamsyanivaran@gmail.com'  
+EMAIL_HOST_USER = **** 
 EMAIL_HOST_PASSWORD = ''
 
